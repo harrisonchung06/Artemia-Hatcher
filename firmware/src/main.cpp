17d05ce20d69291bf4f227915b5f82f74fa1d788
harrisonchung06 <harrison.chung06@gmail.com>
2026-01-08 18:42:02 -0800

    Feature, Firmware, Turn built in LED on or off depending on the reading from an external button

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,14 +2,22 @@
 
 int d = 1000;
 const int button_pin = 7; 
+int button_state; 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
-  Serial.begin(9600); 
+  //Serial.begin(9600); 
 }
 
 void loop() {
-  Serial.println(digitalRead(button_pin)); 
+  //Serial.println(digitalRead(button_pin)); 
+  button_state = digitalRead(button_pin);
+  if (button_state == HIGH){
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else{
+    digitalWrite(LED_BUILTIN, LOW); 
+  }
+   
 }
 
