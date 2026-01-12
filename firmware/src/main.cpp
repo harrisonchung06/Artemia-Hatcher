46768a5985507c8089ced9ed17fb2e15a1af9447
harrisonchung06 <harrison.chung06@gmail.com>
2026-01-12 12:44:23 -0800

    Firmware Update

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,46 @@
 #include <Arduino.h>
 
+//A motor is the drain, B motor is the output, C motor is the input  
+
+#define enA 5
+#define enB 6
+#define enC 11 
+
+#define inA1 7
+#define inA2 8
+
+#define inB1 9
+#define inB2 10
+
+#define inC1 12
+#define inC2 2
+
+void initMotorDriver(int en, int in1, int in2, int speed, bool rotCCW); 
+
+void stopMotor(int in1, int in2); 
+
+void startMotor(int in1, int in2, bool rotCCW);
+
+int speedA = 30;
+int speedB = 255;
+int speedC = 255; 
+//Speed between 0 to 255
+
+bool rotA = true; 
+bool rotB = true; 
+bool rotC = false; 
+//Does the motor spin counterclockwise?
+
 int d = 1000;
-const int button_pin = 7; 
+const int button_pin = 4; 
 int button_state; 
+//Button Parameters 
 
 void setup() {
+  initMotorDriver(enA, inA1, inA2, speedA, rotA); 
+  //initMotorDriver(enB, inB1, inB2, speedB, rotB); 
+  //initMotorDriver(enC, inC1, inC2, speedC, rotC); 
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
   //Serial.begin(9600); 
@@ -12,12 +48,48 @@ void setup() {
 
 void loop() {
   //Serial.println(digitalRead(button_pin)); 
+
   button_state = digitalRead(button_pin);
   if (button_state == HIGH){
     digitalWrite(LED_BUILTIN, HIGH);
+    startMotor(inA1, inA2, rotA);
+    //startMotor(inB1, inB2, rotB); 
+    //startMotor(inC1, inC2, rotC);
   } else{
     digitalWrite(LED_BUILTIN, LOW); 
+    stopMotor(inA1, inA2);
+    stopMotor(inB1, inB2);
+    stopMotor(inC1, inC2);
   }
-   
+  delay(1000);
+  
 }
 
+void initMotorDriver(int en, int in1, int in2, int speed, bool rotCCW){
+  pinMode(en, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT); 
+  //Motor driver pins 
+
+  startMotor(in1, in2, rotCCW); 
+
+  analogWrite(en, speed); 
+  // Set the speed 
+}
+
+void stopMotor(int in1, int in2){
+  digitalWrite(in1, LOW); 
+  digitalWrite(in2, LOW);
+  //Stop motors 
+}
+
+void startMotor(int in1, int in2, bool rotCCW){
+  if (rotCCW){
+    digitalWrite(in1, LOW); 
+    digitalWrite(in2, HIGH); 
+  } else{
+    digitalWrite(in1, HIGH); 
+    digitalWrite(in2, LOW); 
+  }
+  //Set rotation direction
+}
