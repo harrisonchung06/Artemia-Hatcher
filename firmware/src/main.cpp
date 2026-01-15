2ef46ea8a4b484d395d92a40a5c975ee592f0f78
harrisonchung06 <harrison.chung06@gmail.com>
2026-01-15 01:22:54 -0800

    Feature, Firmware, Trying to integrate interrupt pin alarm to trigger. Created alarm functions to match local microcontroller time.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -19,6 +19,10 @@
 
 #define CLK_INT 2 
 #define CLK_ADD 0x68
+#define CLK_TIME_REG 0x00
+#define CLK_ALARM1_REG 0x07
+#define CLK_CONTROL_REG 0x0E
+#define CLK_STATUS_REG 0x0F
 
 void initMotorDriver(int en, int in1, int in2, int speed, bool rotCCW); 
 
@@ -30,11 +34,26 @@ void sleepTimer(int time); //Timer function in minutes, how often to check time
 
 void setClockZero();
 
+void setAlarm1(byte seconds, byte minutes, byte hours);
+
+void clearAlarm1(); 
+
+void onInterrupt();
+
+void unarmAlarm1();
+
+void armAlarm1();
+
+uint8_t readRegister(uint8_t reg);
+
+void writeRegister(uint8_t reg, uint8_t val);
+
 byte decToBcd(byte data); 
 
 byte bcdToDec(byte data); 
 
 byte* getLocalTime();
+
 
 int speedA = 30;
 int speedB = 255;
@@ -57,10 +76,12 @@ float flowRate = 0.06; //Liters per minute
 byte reg[3] = {0x00,0x01,0x02};
 byte val[3]; 
 byte* ptr; 
-int curr_time = 0; //Timer variable 
 
 void setup() {
-  //initMotorDriver(enA, inA1, inA2, speedA, rotA); 
+  Serial.begin(9600); 
+  Wire.begin();
+
+  initMotorDriver(enA, inA1, inA2, speedA, rotA); 
   initMotorDriver(enB, inB1, inB2, speedB, rotB); 
   initMotorDriver(enC, inC1, inC2, speedC, rotC); 
 
@@ -70,21 +91,19 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(button_pin, INPUT_PULLUP);
-  
-  Wire.begin();
+
+  pinMode(CLK_INT, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(CLK_INT), onInterrupt, FALLING); 
 
   setClockZero(); 
-
-  Serial.begin(9600); 
 }
 
 void loop() {
-
-  ptr = getLocalTime();
-  Serial.print(ptr[0],DEC);
-  Serial.print(ptr[1],DEC);
-  Serial.println(ptr[2],DEC);
-  delay(1000); 
+  setClockZero(); 
+  setAlarm1(5,0,0);
+  delay(5000);
+  Serial.println("Done");
+  Serial.println(digitalRead(CLK_INT));
   //Serial.println(digitalRead(button_pin)); 
   /*
   button_state = digitalRead(button_pin);
@@ -145,6 +164,11 @@ void startMotor(int in1, int in2, bool rotCCW){
   }
   //Set rotation direction
 }
+//Motor Functions
+
+void onInterrupt(){
+  Serial.println("Wake up!!!"); 
+}
 
 void sleepTimer(int time){
 
@@ -163,8 +187,8 @@ void sleepTimer(int time){
 
 void setClockZero(){ //Set microcontroller clock to zero 
   Wire.beginTransmission(CLK_ADD);
-
-  Wire.write(0x00);
+  
+  Wire.write(CLK_TIME_REG);
   Wire.write(decToBcd(0));
   //Write seconds 
   Wire.write(decToBcd(0));
@@ -191,6 +215,77 @@ byte decToBcd(byte data){
 byte bcdToDec(byte data){
   return ( (data/16*10) + (data%16) );
 }
-//Helper Functions 
+
+void setAlarm1(byte second, byte minute, byte hour){
+  byte dydw = 0b10000000; 
+  //Ignore DWDY matching (Mask bit)
+
+  unarmAlarm1(); 
+  clearAlarm1();  
+  //Unarm and clear alarm
+
+  second = decToBcd(second);
+  minute = decToBcd(minute);
+  hour = decToBcd(hour); 
+  //Convert to Bit
+
+  second &= 0b01111111;
+  minute &= 0b01111111;
+  hour &= 0b01111111;
+  //Bitwise operator to enable matching (Mask bit)
+  Wire.write(CLK_ADD);
+  Wire.write(CLK_ALARM1_REG);
+  Wire.write(second);
+  Wire.write(minute);
+  Wire.write(hour);
+  Wire.write(dydw);
+  Wire.endTransmission();
+  //Write to alarm register 
+
+  armAlarm1(); 
+  //Arm alarm 
+}
+
+void clearAlarm1(){
+  uint8_t val;
+  val = readRegister(CLK_STATUS_REG);
+  val &= 0b11111110;
+  writeRegister(CLK_STATUS_REG, val);
+}
+
+void armAlarm1(){
+  uint8_t val;
+  val = readRegister(CLK_CONTROL_REG); 
+  val |= 0b00000001; //A1IE
+  val |= 0b00000100; //INTCN
+  writeRegister(CLK_CONTROL_REG, val);
+}
+
+void unarmAlarm1(){
+  uint8_t val;
+  val = readRegister(CLK_CONTROL_REG); 
+  val &= 0b11111110;
+  writeRegister(CLK_CONTROL_REG, val); 
+}
+
+uint8_t readRegister(uint8_t reg){
+  uint8_t val;
+  Wire.beginTransmission(CLK_ADD);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(CLK_ADD, 1);
+  val = Wire.read();
+  return val; 
+}
+
+void writeRegister(uint8_t reg, uint8_t val){
+  Wire.beginTransmission(CLK_ADD);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+//Timer Functions 
+
 
 
