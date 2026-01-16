33cfe32bedba0c1639440548361bfcd5fa8ac516
harrisonchung06 <harrison.chung06@gmail.com>
2026-01-15 23:18:23 -0800

    Features, Firmware, Enable interrupt on pin 4 with switch so that the program async listens for the switch. When the switch is off it turns everything off and sleeps the microcontroller

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,9 +2,13 @@
 #include <Wire.h>
 #include <LowPower.h>
 
-//A motor is the drain, B motor is the yield, C motor is the input water source   
+/*
+A motor is the drain
+B motor is the yield
+C motor is the input water source   
+*/
 
-//Define Pinout
+//Define motor driver pinout
 #define enA 5
 #define enB 6
 #define enC 11 
@@ -16,8 +20,12 @@
 #define inB2 10
 
 #define inC1 12
-#define inC2 3
+#define inC2 4
 
+//Define button pinout 
+#define BUTTON 3
+
+//Define clock pinout 
 #define CLK_INT 2 
 #define CLK_ADD 0x68
 #define CLK_TIME_REG 0x00
@@ -25,13 +33,16 @@
 #define CLK_CONTROL_REG 0x0E
 #define CLK_STATUS_REG 0x0F
 
+//Prototype functions 
+
 void initMotorDriver(int en, int in1, int in2, int speed, bool rotCCW); 
 
 void stopMotor(int in1, int in2); 
 
 void startMotor(int in1, int in2, bool rotCCW);
 
-void sleepTimer(int secs, int mins, int hours); //Timer function in minutes
+//Timer function in minutes
+void sleepTimer(int secs, int mins, int hours); 
 
 void setClockZero();
 
@@ -40,6 +51,8 @@ void setAlarm1(byte seconds, byte minutes, byte hours);
 void clearAlarm1(); 
 
 void onInterrupt();
+
+void onInterruptButton();
 
 void unarmAlarm1();
 
@@ -53,29 +66,28 @@ byte decToBcd(byte data);
 
 byte bcdToDec(byte data); 
 
-
+//Speed between 0 to 255
 int speedA = 30;
 int speedB = 255;
 int speedC = 255; 
-//Speed between 0 to 255
 
+//Does the motor spin counterclockwise?
 bool rotA = false; 
 bool rotB = true; 
 bool rotC = false; 
-//Does the motor spin counterclockwise?
 
-int d = 1000;
-const int button_pin = 4; 
-int button_state; 
-//Button Parameters 
+//Button Parameter 
+int buttonState;
 
-float v = 1.33; //Volume in liters 
-float flowRate = 0.06; //Liters per minute 
+//Volume in liters 
+float v = 1.33;
+//Liters per minute 
+float flowRate = 0.06;
 
 void setup() {
   Serial.begin(9600); 
   Wire.begin();
-
+  //Motors 
   initMotorDriver(enA, inA1, inA2, speedA, rotA); 
   initMotorDriver(enB, inB1, inB2, speedB, rotB); 
   initMotorDriver(enC, inC1, inC2, speedC, rotC); 
@@ -85,17 +97,20 @@ void setup() {
   stopMotor(inC1, inB2); 
 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(button_pin, INPUT_PULLUP);
-
+  
+  //Interrupts
   pinMode(CLK_INT, INPUT_PULLUP); 
   attachInterrupt(digitalPinToInterrupt(CLK_INT), onInterrupt, FALLING); 
+
+  pinMode(BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), onInterruptButton, LOW); 
 
   setClockZero(); 
 }
 
 void loop() {
-  button_state = digitalRead(button_pin);
-  if (button_state == HIGH){
+  buttonState = digitalRead(BUTTON);
+  if (buttonState == HIGH){
     digitalWrite(LED_BUILTIN, HIGH);
     startMotor(inC1, inC2, rotC);
     sleepTimer(5,0,0); //sleepTimer(0,9,0); 
@@ -110,93 +125,95 @@ void loop() {
     startMotor(inB1, inB2, rotB); //Yield 
     sleepTimer(5,0,0); //sleepTimer(0,1,0);
     stopMotor(inB1, inB2);
-    
+
+    //Wait for manual reset 
     sleepTimer(0,0,999); 
-  } else{
-    digitalWrite(LED_BUILTIN, LOW); 
-    stopMotor(inA1, inA2);
-    stopMotor(inB1, inB2);
-    stopMotor(inC1, inC2);
   }
 }
 
+//Motor Functions
 void initMotorDriver(int en, int in1, int in2, int speed, bool rotCCW){
+  //Motor driver pins 
   pinMode(en, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT); 
-  //Motor driver pins 
 
   startMotor(in1, in2, rotCCW); 
-
-  analogWrite(en, speed); 
   // Set the speed 
+  analogWrite(en, speed); 
+  
 }
 
 void stopMotor(int in1, int in2){
+  //Stop motors
   digitalWrite(in1, LOW); 
   digitalWrite(in2, LOW);
-  //Stop motors 
 }
 
 void startMotor(int in1, int in2, bool rotCCW){
+  //Set rotation direction
   if (rotCCW){
     digitalWrite(in1, LOW); 
     digitalWrite(in2, HIGH); 
   } else{
     digitalWrite(in1, HIGH); 
     digitalWrite(in2, LOW); 
-  }
-  //Set rotation direction
+  } 
 }
-//Motor Functions
 
+//Timer Functions
 void sleepTimer(int secs, int mins, int hours){
   setClockZero();
   setAlarm1(secs, mins, hours); 
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); //Low power 
+  //Low power 
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
 }
 
-void setClockZero(){ //Set microcontroller clock to zero 
+//Set microcontroller clock to zero 
+void setClockZero(){ 
   Wire.beginTransmission(CLK_ADD);
   
   Wire.write(CLK_TIME_REG);
-  Wire.write(decToBcd(0));
   //Write seconds 
   Wire.write(decToBcd(0));
   //Write minutes
   Wire.write(decToBcd(0));
-  //Write Hours 
+  //Write Hours
+  Wire.write(decToBcd(0));
+   
   Wire.endTransmission(); 
 }
 
 void setAlarm1(byte second, byte minute, byte hour){
-  byte dydw = 0b10000000; 
   //Ignore DWDY matching (Mask bit)
-
+  byte dydw = 0b10000000; 
+  
+  //Unarm and clear alarm
   unarmAlarm1(); 
   clearAlarm1();  
-  //Unarm and clear alarm
-
+ 
+  //Convert to Bit
   second = decToBcd(second);
   minute = decToBcd(minute);
   hour = decToBcd(hour); 
-  //Convert to Bit
-
+  
+  //Bitwise operator to enable matching (Mask bit)
   second &= 0b01111111;
   minute &= 0b01111111;
   hour &= 0b01111111;
-  //Bitwise operator to enable matching (Mask bit)
-  Wire.beginTransmission(CLK_ADD); 
+  
+  //Write to alarm register
+  Wire.beginTransmission(CLK_ADD); //Begin Transmission
   Wire.write(CLK_ALARM1_REG);
   Wire.write(second);
   Wire.write(minute);
   Wire.write(hour);
   Wire.write(dydw);
   Wire.endTransmission();
-  //Write to alarm register 
 
-  armAlarm1(); 
   //Arm alarm 
+  armAlarm1(); 
+  
 }
 
 void clearAlarm1(){
@@ -243,12 +260,18 @@ byte decToBcd(byte data){
 }
 byte bcdToDec(byte data){
   return ( (data/16*10) + (data%16) );
+} 
+
+//Service Functions
+void onInterrupt(){
 }
 
-//Timer Functions 
-
-void onInterrupt(){
-  //Service Function
+void onInterruptButton(){
+  digitalWrite(LED_BUILTIN, LOW); 
+  stopMotor(inA1, inA2);
+  stopMotor(inB1, inB2);
+  stopMotor(inC1, inC2);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF,BOD_OFF);
 }
 
 
